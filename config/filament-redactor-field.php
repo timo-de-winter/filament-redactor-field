c3b17092eca4b683127ba876b5a5625502f2e1c2
timo-de-winter <52080998+timo-de-winter@users.noreply.github.com>
2026-07-27 08:51:18 +0000

    Fix styling

--- config/filament-redactor-field.php
+++ config/filament-redactor-field.php
@@ -1,25 +1,27 @@
 <?php
 
+use TimoDeWinter\FilamentRedactorField\Enums\DefaultRedactorPlugin;
+
 // config for TimoDeWinter/FilamentRedactorField
 return [
     'darkmode_enabled' => true,
 
     'plugins' => [
-        \TimoDeWinter\FilamentRedactorField\Enums\DefaultRedactorPlugin::Alignment,
-        \TimoDeWinter\FilamentRedactorField\Enums\DefaultRedactorPlugin::BlockBackground,
-        \TimoDeWinter\FilamentRedactorField\Enums\DefaultRedactorPlugin::BlockBorder,
-        \TimoDeWinter\FilamentRedactorField\Enums\DefaultRedactorPlugin::BlockColor,
-        \TimoDeWinter\FilamentRedactorField\Enums\DefaultRedactorPlugin::BlockFontsize,
-        \TimoDeWinter\FilamentRedactorField\Enums\DefaultRedactorPlugin::BlockSpacing,
-        \TimoDeWinter\FilamentRedactorField\Enums\DefaultRedactorPlugin::Emoji,
-        \TimoDeWinter\FilamentRedactorField\Enums\DefaultRedactorPlugin::FontColor,
-        \TimoDeWinter\FilamentRedactorField\Enums\DefaultRedactorPlugin::FontFamily,
-        \TimoDeWinter\FilamentRedactorField\Enums\DefaultRedactorPlugin::FontSize,
-        \TimoDeWinter\FilamentRedactorField\Enums\DefaultRedactorPlugin::FullScreen,
-        \TimoDeWinter\FilamentRedactorField\Enums\DefaultRedactorPlugin::Icons,
-        \TimoDeWinter\FilamentRedactorField\Enums\DefaultRedactorPlugin::ImageResize,
-        \TimoDeWinter\FilamentRedactorField\Enums\DefaultRedactorPlugin::Limiter,
-        \TimoDeWinter\FilamentRedactorField\Enums\DefaultRedactorPlugin::SpecialChars,
-        \TimoDeWinter\FilamentRedactorField\Enums\DefaultRedactorPlugin::TextExpander,
+        DefaultRedactorPlugin::Alignment,
+        DefaultRedactorPlugin::BlockBackground,
+        DefaultRedactorPlugin::BlockBorder,
+        DefaultRedactorPlugin::BlockColor,
+        DefaultRedactorPlugin::BlockFontsize,
+        DefaultRedactorPlugin::BlockSpacing,
+        DefaultRedactorPlugin::Emoji,
+        DefaultRedactorPlugin::FontColor,
+        DefaultRedactorPlugin::FontFamily,
+        DefaultRedactorPlugin::FontSize,
+        DefaultRedactorPlugin::FullScreen,
+        DefaultRedactorPlugin::Icons,
+        DefaultRedactorPlugin::ImageResize,
+        DefaultRedactorPlugin::Limiter,
+        DefaultRedactorPlugin::SpecialChars,
+        DefaultRedactorPlugin::TextExpander,
     ],
 ];
